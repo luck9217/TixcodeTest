2e5b499d1f598aff13f449f282a4159bae3b2fe6
Lucas Chavez <luck9217@gmail.com>
2021-09-29 13:11:59 +0200

    feat: add api data using axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import UserTable from './components/tables/UserTable'
 import AddUserForm from './components/tables/AddUserForm'
+import axios from 'axios';
 
 const App = () => {
   const usersData = [
@@ -9,11 +10,11 @@ const App = () => {
     { id: 3, mail: 'asda@gmail.com', edad: '15',datarandom:'random',datecreation:'01/02/2003' },
   ]
 
-  const initialFormState = {id:null, mail: '', edad: '',datarandom:'',datecreation:'' }
+  // const initialFormState = {id:null, mail: '', edad: '',datarandom:'',datecreation:'' }
 
   //Setting state
   const [users, setUsers] = useState(usersData)
-  const [ currentUser, setCurrentUser ] = useState(initialFormState)
+  // const [ currentUser, setCurrentUser ] = useState(initialFormState)
 
   //CRUD operations
   const addUser = (user) => {
@@ -27,6 +28,32 @@ const App = () => {
 		setUsers(users.filter(user => user.id !== id))
 	}
 
+  useEffect(() => {
+    if(addUser) {
+
+       console.log("click")
+
+       const apifunction = async () => {
+
+      //   //evitamos la ejecucion la primera vez
+      // if(moneda==='')return;
+  
+      //consultar la api para obetener la cotizacion
+      const url=`https://rickandmortyapi.com/api/character/10`;
+      
+      const resultado= await axios.get(url);
+  
+        //guardar cotizacion
+      console.log(resultado.data);
+     
+         
+       }
+  
+       apifunction();
+
+      }
+    }
+  )
 
 
   return (
